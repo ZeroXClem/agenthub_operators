9355235e229abb1e167d95610d0e3f39a5c3c785
MaxBrodeurUrbas <max@garnr.io>
2023-06-01 19:21:05 -0700

    Allow previously uploadede pdf's to be ingested

--- operators/ingest_pdf.py
+++ operators/ingest_pdf.py
@@ -29,6 +29,11 @@ class IngestPDF(BaseOperator):
                 "name": "pdf_uri",
                 "data_type": "string",
                 "placeholder": "Enter the URL of the PDF"
+            },
+            {
+                "name": "uploaded_file_name",
+                "data_type": "string",
+                "placeholder": "Enter the name.pdf of the uploaded PDF"
             }
             # TODO: uncomment when there are more than 1 methods of parsing PDF.
             #, 
@@ -65,13 +70,17 @@ class IngestPDF(BaseOperator):
     ):
         params = step['parameters']
         pdf_uri = params.get('pdf_uri')
+        uploaded_file_name = params.get('uploaded_file_name')
         file_name = ai_context.get_input('file_name', self)
-        self.ingest(pdf_uri, file_name, ai_context)
+        self.ingest(pdf_uri, file_name, uploaded_file_name, ai_context)
 
-    def ingest(self, pdf_uri, file_name, ai_context):
+    def ingest(self, pdf_uri, file_name, uploaded_file_name, ai_context):
+        # Uploaded file name is used to ingest a file previously uploaded to your workspace. 
+        if uploaded_file_name:
+            file_data = self.load_pdf_from_storage(uploaded_file_name, False, ai_context)
         # If file name has been provided via input, it takes precedence over pdf_uri
-        if file_name:
-            file_data = self.load_pdf_from_storage(file_name, ai_context)
+        elif file_name:
+            file_data = self.load_pdf_from_storage(file_name, True, ai_context)
 
         elif self.is_url(pdf_uri):
             file_data = self.load_pdf_from_uri(pdf_uri)
@@ -89,26 +98,12 @@ class IngestPDF(BaseOperator):
         response = requests.get(url)
         response.raise_for_status()  # Ensure we got a valid response
         return response.content
-
-    # def download_file(self, file_name, ai_context):
-    #     # Use the tempfile module to create a temporary directory
-    #     temp_dir = tempfile.mkdtemp()
-
-    #     # Create the full path to the temporary file
-    #     local_file_path = os.path.join(temp_dir, file_name)
-
-    #     # Get the file data from ai_context
-    #     file_data = ai_context.get_file(file_name, ai_context.get_run_id())
-
-    #     # Write the file data to the temporary file
-    #     with open(local_file_path, 'wb') as temp_file:
-    #         temp_file.write(file_data)
-        
-    #     # Return the path to the temporary file
-    #     return local_file_path
     
-    def load_pdf_from_storage(self, file_name, ai_context):
-        file_data = ai_context.get_file(file_name, ai_context.get_run_id())
+    def load_pdf_from_storage(self, file_name, generated_this_run, ai_context):
+        if generated_this_run:
+            file_data = ai_context.get_file(file_name, ai_context.get_run_id())
+        else:
+            file_data = ai_context.get_file(file_name)
         return file_data
     
     def read_pdf(self, pdf):
@@ -119,12 +114,4 @@ class IngestPDF(BaseOperator):
         
         return pdf_content
     
-        # Make sure Pandas data frames don't truncate cells of tables inside of PDF document.
-        # pd.set_option('display.max_colwidth', None)
-        # response = requests.get(params['pdf_uri'])
-        # pdf_content = io.BytesIO(response.content)
-        # df_list = tabula.read_pdf(pdf_content, pages='all')
-        # pdf_content = "\n".join(df.to_string(index=False) for df in df_list)
-        # #ai_context.add_to_log(f'PDF content: {pdf_content}')
-        # ai_context.set_output('pdf_content', pdf_content, self)
         
